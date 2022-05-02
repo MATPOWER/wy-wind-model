cb5f58d5a0b727586f9d4ebe5506a0b9d1bd89cc
Ray Zimmerman <rz10@cornell.edu>
2022-05-02 13:06:16 -0500

    Add tests for pidx0+1.
    For realizations, they values of shifted, for forecasts, they are completely different.

--- lib/t/t_wy_wind.m
+++ lib/t/t_wy_wind.m
@@ -28,7 +28,7 @@ dt0 = [2004 1 1 1 0 0]; %% date of first period in wind data, 2004-01-01 1:00am
 dt = [2004 8 1 0 0 0];  %% date of period of interest, 2004-08-01 12:00am
 pidx0 = wy_wind_date2pidx(npd, dt0, dt);    %% scalar index of period of interest
 
-t_begin(5+np, quiet);
+t_begin(13+np, quiet);
 
 %% load the historical data
 % wind_data = load('winddata_npcc');  %% 26303 x 16
@@ -46,13 +46,16 @@ tp = wy_wind_trans_probs(model, np, bins);
 
 %% wind speed realization from data
 wsr_data = wy_wind_realizations(log_wind_data, widx, pidx0, np);
+wsr_data1 = wy_wind_realizations(log_wind_data, widx, pidx0+1, np);
 wsr_data = (10.^wsr_data)-1;    %% convert log(wind+1) to wind speed
+wsr_data1 = (10.^wsr_data1)-1;  %% convert log(wind+1) to wind speed
 
 %% convert realization from speed to power
 s2p = load('WindPowerCurveIEC.txt');
 s2p = s2p(:, 6)';       %% select curve for multi-turbine
 % wpr = wy_wind_speed2power(wsr_model, s2p);
 wpr_data = wy_wind_speed2power(wsr_data, 5);
+wpr_data1 = wy_wind_speed2power(wsr_data1, 5);
 
 % %% wind speed realization from model
 % wsr_model = wy_wind_realizations(model, widx, pidx0, np);
@@ -64,14 +67,17 @@ wpr_data = wy_wind_speed2power(wsr_data, 5);
 %% generate forecast
 ws0 = log_wind_data(pidx0, widx);
 wsf = wy_wind_forecasts(model, widx, pidx0, ws0, np, bins);
+wsf1 = wy_wind_forecasts(model, widx, pidx0+1, ws0, np, bins);
 wsf = (10.^wsf)-1;      %% convert log(wind+1) to wind speed
+wsf1 = (10.^wsf1)-1;    %% convert log(wind+1) to wind speed
 
 %% convert forecast from speed to power
 % wpf = wy_wind_speed2power(wsf, s2p);
 wpf = wy_wind_speed2power(wsf, 5);
+wpf1 = wy_wind_speed2power(wsf1, 5);
 
 %% load results
-% save t_wy_wind_results tp wsr_data wpr_data wsf wpf
+% save t_wy_wind_results tp wsr_data wpr_data wsf wpf wsr_data1 wpr_data1 wsf1 wpf1
 s = load('t_wy_wind_results');
 
 t = 'wy_wind_trans_probs : ';
@@ -80,17 +86,25 @@ for p = 1:np
     t_is(tp{p}, s.tp{p}, 12, sprintf('%stp{%d}', t, p));
 end
 
-t = 'wy_wind_realizations(data, ...)';
-t_is(wsr_data, s.wsr_data, 12, t);
+t = 'wy_wind_realizations(data, ...) : ';
+t_is(wsr_data, s.wsr_data, 12, [t 'wsr_t0']);
+t_is(wsr_data1, s.wsr_data1, 12, [t 'wsr_t1']);
+t_is(wsr_data(2:end, :), wsr_data1(1:end-1, :), 12, [t 'wsr_t0(2:end,:) == wsr_t1(1:end-1,:)']);
 
-t = 'wy_wind_speed2power';
-t_is(wpr_data, s.wpr_data, 12, t);
+t = 'wy_wind_speed2power : ';
+t_is(wpr_data, s.wpr_data, 12, [t 'wpr_t0']);
+t_is(wpr_data1, s.wpr_data1, 12, [t 'wpr_t1']);
+t_is(wpr_data(2:end, :), wpr_data1(1:end-1, :), 12, [t 'wpr_t0(2:end,:) == wpr_t1(1:end-1,:)']);
 
-t = 'wy_wind_forecasts';
-t_is(wsf, s.wsf, 12, t);
+t = 'wy_wind_forecasts : ';
+t_is(wsf, s.wsf, 12, [t 'wsf_t0']);
+t_is(wsf1, s.wsf1, 12, [t 'wsf_t1']);
+t_ok(norm(wsf(2:end, :, 1) - wsf1(1:end-1, :, 1)) > 1, [t 'wsf_t0(2:end,:) ~= wsf_t1(1:end-1,:)']);
 
-t = 'wy_wind_speed2power';
-t_is(wpf, s.wpf, 12, t);
+t = 'wy_wind_speed2power : ';
+t_is(wpf, s.wpf, 12, [t 'wpf_t0']);
+t_is(wpf1, s.wpf1, 12, [t 'wpf_t1']);
+t_ok(norm(wpf(2:end, :, 1) - wpf1(1:end-1, :, 1)) > 0.1, [t 'wpf_t0(2:end,:,1) ~= wpf_t1(1:end-1,:,1)']);
 
 if have_feature('octave')
     warning(s1.state, file_in_path_warn_id);
